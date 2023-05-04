5f7ef2441f7a3036197f728832ce9a10c519dd85
oknod <114230918+oknod@users.noreply.github.com>
2023-05-04 15:22:50 +0300

    hw2 done

---
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,77 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    private int size;
+
+    public void clear() {
+
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist ");
+        } else if (size == storage.length) {
+            System.out.println("Storage overflow");
+        } else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    public void update(Resume r) {
+        if (getIndex(r.getUuid()) == -1) {
+            System.out.println("Resume " + r.getUuid() + " not found");
+        } else {
+            storage[getIndex(r.getUuid())] = r;
+        }
+    }
+
+    public Resume get(String uuid) {
+        if (getIndex(uuid) == -1) {
+            System.out.println("Resume " + uuid + " not found");
+            return null;
+        }
+        return storage[getIndex(uuid)];
+
+    }
+
+    public void delete(String uuid) {
+        if (getIndex(uuid) == -1) {
+            System.out.println("Resume" + uuid + "not found");
+        } else {
+            storage[getIndex(uuid)] = storage[size - 1];
+            size--;
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].getUuid()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
